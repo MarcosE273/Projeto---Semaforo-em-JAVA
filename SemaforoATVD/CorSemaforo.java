744eb6c73745f1f654a315a4d623b07c7d0c7ea7
Marcos Eduardo <54457118+MarcosE273@users.noreply.github.com>
2022-10-19 19:34:09 -0300

    Add files via upload

---
+++ SemaforoATVD/CorSemaforo.java
@@ -0,0 +1,16 @@
+package SemaforoATVD;
+
+public enum CorSemaforo {
+	
+	VERDE(2000), AMARELO(300), VERMELHO(1000);
+	
+	private int tempoEspera;
+	
+	CorSemaforo(int tempoEspera){
+		this.tempoEspera = tempoEspera;
+	}
+
+	public int getTempoEspera() {
+		return tempoEspera;
+	}
+}
